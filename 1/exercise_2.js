e34f244ee5fd7ca24ce7ffac64cee7ebe084ca9b
Elena2004create <q62523474@gmail.com>
2024-10-05 02:28:23 +0300

    Исправленный код

--- 1/exercise_2.js
+++ 1/exercise_2.js
@@ -7,10 +7,10 @@ let owner;
 let someID = Symbol('id'); 
 let bigNumber = 12345678901234567890n; 
 
-console.log(petName, typeof userName); 
+console.log(petName, typeof petName); 
 console.log(age, typeof age); 
-console.log(isCat, typeof isStudent); 
-console.log(nullCheck, typeof user); 
-console.log(owner, typeof address); 
-console.log(someID, typeof userID); 
+console.log(isCat, typeof isCat); 
+console.log(nullCheck, typeof nullCheck); 
+console.log(owner, typeof owner); 
+console.log(someID, typeof someID); 
 console.log(bigNumber, typeof bigNumber);
